b725fcd9bd16b6543a5254ec280825f0f47c2ecf
reddwarf405 <138400133+reddwarf405@users.noreply.github.com>
2024-06-12 12:05:38 -0500

    Added player FOV.

--- game_map.py
+++ game_map.py
@@ -8,9 +8,21 @@ class GameMap:
         self.width, self.height = width, height
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
 
+        self.visible=np.full((width, height), fill_value=False, order="F") # Tiles the player can see currently
+        self.explored=np.full((width, height), fill_value=False, order="F") # Tiles the player has seen before
+
+
     def in_bounds(self, x: int, y: int) -> bool:
         # Checks if something is in the bounds of the map & returns true if so
         return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console) -> None:
-        console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+        # Renders the map.
+        # If a tile is 'visible', it will be drawn with light colors
+        # If it isn't, but it has been explored, draw it with 'dark' colors
+        # Otherwise it will default to SHROUD
+        console.tiles_rgb[0:self.width, 0:self.height] = np.select(
+            condlist=[self.visible, self.explored],
+            choicelist=[self.tiles["light"], self.tiles["dark"]],
+            default=tile_types.SHROUD
+        )
--- tile_types.py
+++ tile_types.py
@@ -17,6 +17,7 @@ tile_dt = np.dtype(
         ("walkable", np.bool_),  # True if this tile can be walked over.
         ("transparent", np.bool_),  # True if this tile doesn't block FOV.
         ("dark", graphic_dt),  # Graphics for when this tile is not in FOV.
+        ("light", graphic_dt) # Graphics for when this tile IS in FOV
     ]
 )
 
@@ -25,8 +26,12 @@ def new_tile(
         walkable: int,
         transparent: int,
         dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+        light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:
-    return np.array((walkable, transparent, dark), dtype=tile_dt)
+    return np.array((walkable, transparent, dark, light), dtype=tile_dt)
 
-floor = new_tile(walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)),)
-wall = new_tile(walkable=False, transparent=False, dark=(ord("#"), (255, 255, 255), (0, 0, 100)),)
+#SHROUD will represent unseen and unexplored tiles
+SHROUD = np.array((ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt)
+
+floor = new_tile(walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)), light=(ord(" "), (255, 255, 255), (200, 180, 50)))
+wall = new_tile(walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (0, 0, 100)), light=(ord(" "), (255, 255, 255), (130, 110, 50)))
